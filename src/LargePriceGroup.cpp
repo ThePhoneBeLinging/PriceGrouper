2e02d4c4b8df17f573c576893a14506f000d372c
EAL <elias@takefive.dk>
2025-03-10 18:10:44 +0100

    feat: smallPriceGroups are now merged if they are in the same column

--- src/LargePriceGroup.cpp
+++ src/LargePriceGroup.cpp
@@ -6,12 +6,24 @@
 
 void LargePriceGroup::addSmallPriceGroup(const std::shared_ptr<SmallPriceGroup>& smallPriceGroup)
 {
+    if (not smallPriceGroups_.empty())
+    {
+        if (smallPriceGroup->getStartTime() == smallPriceGroups_.back()->getEndTime() && smallPriceGroups_.back()->getStartTime() != -1)
+        {
+            smallPriceGroups_.back()->appendSmallPriceGroup(smallPriceGroup);
+            return;
+        }
+    }
     smallPriceGroups_.push_back(smallPriceGroup);
+
 }
 
 void LargePriceGroup::setSmallPriceGroup(const std::vector<std::shared_ptr<SmallPriceGroup>>& smallPriceGroups)
 {
-    smallPriceGroups_ = smallPriceGroups;
+    for (const auto& price : smallPriceGroups)
+    {
+        addSmallPriceGroup(price);
+    }
 }
 
 std::vector<std::shared_ptr<SmallPriceGroup>> LargePriceGroup::getSmallPriceGroups() const
